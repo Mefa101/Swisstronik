789d2a9ecbbe13e63134754fa80804b4e5e7e3ec
Mefa101 <141573555+Mefa101@users.noreply.github.com>
2024-07-28 10:30:04 +0700

    Create SimpleStorageV2.sol

---
+++ contracts/proxy/SimpleStorageV2.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorageV2 {
+    uint256 public value;
+
+    function set(uint256 _value) public {
+        value = _value;
+    }
+
+    function increment() public {
+        value++;
+    }
+}
